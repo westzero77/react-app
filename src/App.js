6f44c0f108239f50cdccfbdbbe11caa967d256fc
westzero77 <westzero713@gmail.com>
2024-04-06 19:37:05 +0900

    pure.html 을 리액트 코드로 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 import {Component} from "react";
 
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>WEB</h1>
+        world wide web!
+      </header>
+    );
+  }
+}
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Article extends Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </article>
+    );
+  }
+}
+
+// 유사 Javascript 인 JSX 문법
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, React!!
+        <Subject></Subject>
+        <Nav></Nav>
+        <Article></Article>
       </div>
     );
   }
